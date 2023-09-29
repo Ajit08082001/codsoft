7656f220008527e47c2dfac1822b46ad298fd7db
Ajit08082001 <ajitojha0808@gmail.com>
2023-09-29 15:20:21 +0530

    First commit

---
+++ internship -level1/calculator/script.js
@@ -0,0 +1,24 @@
+let Result=document.getElementById('inputtext');
+
+let calculate=(number)=>{
+  Result.value=Result.value+number;
+}
+
+
+let result=()=>{
+try{
+    Result.value=eval(Result.value)
+}
+catch(err)
+{
+    alert("Enter the valid value");
+}
+}
+
+function clr(){
+    Result.value=" ";
+}
+
+function del(){
+    Result.value=Result.value.slice(0,-1);
+}
